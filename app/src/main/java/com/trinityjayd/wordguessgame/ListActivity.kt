278a6813e143b9c22dd49eb785f87368a08e007a
TrinityJayd <trinitytiratram@gmail.com>
2023-03-13 07:29:18 +0000

    added 3rd activity. Recycler view completed

--- app/src/main/java/com/trinityjayd/wordguessgame/ListActivity.kt
+++ app/src/main/java/com/trinityjayd/wordguessgame/ListActivity.kt
@@ -2,10 +2,33 @@ package com.trinityjayd.wordguessgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ListActivity : AppCompatActivity() {
+
+    private val itemsList = ArrayList<String>()
+    private lateinit var customAdapter: CustomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        title = "RecyclerView"
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        customAdapter = CustomAdapter(itemsList)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = customAdapter
+        prepareItems()
+    }
+
+    private fun prepareItems() {
+        //Create an instance of the word class, get the lst of words and add it to the itemsList
+        val wordObj = Words()
+        val fruits = wordObj.getWords()
+        itemsList.addAll(fruits)
+        customAdapter.notifyDataSetChanged()
     }
 }
